ad342a4abc8072d08c57a44b4e4c2312e51bc218
Raj kumar Jana <117941938+rajconfig@users.noreply.github.com>
2026-06-29 16:37:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,5 +7,14 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  default = "my-key"
+  description = "Existing AWS Key Pair Name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
 }
